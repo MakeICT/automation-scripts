7d9b21590a68e752abef5b8b8bc56cd387fb2ae6
Christian Kindel <iceman81292@gmail.com>
2022-12-06 09:20:59 -0600

    update waiver script to fetch a date range rather than last 100 waivers

--- WaiverCheck.py
+++ WaiverCheck.py
@@ -55,89 +55,106 @@ class ChildScript(Script):
 		# 	print(template.template_id + ': ' + template.title)
 
 		# Get a list of recent signed waivers for this account
-		summaries = sw.get_waiver_summaries(100)
 
-		for summary in summaries:
-			print("====================================")
-			print(summary.waiver_id + ': ' + summary.title)
+		from datetime import timezone
+		# from_dt = datetime(2022, 7, 6, tzinfo=timezone.utc)
+		# to_dt = datetime(2022, 10, 6, tzinfo=timezone.utc)
 
-			WA_ID = None
-			print(summary.first_name, summary.last_name)
+		to_dt = datetime.today().astimezone(timezone.utc)
+		from_dt = to_dt - timedelta(days=14)
+ 
+		while True:
+			summaries = sw.get_waiver_summaries(100, from_dts=from_dt.isoformat(), to_dts=to_dt.isoformat())
+			# summaries = sw.get_waiver_summaries(100)
 
-			for tag in summary.tags:
-				if tag.split(' ')[0]=='WA_ID':
-					WA_ID = int(tag.split(' ')[1])
+			for summary in summaries:
+				print("====================================")
+				print(summary.waiver_id + ': ' + summary.title)
 
-			sw_fails = 0
-			while(1):
-				try:
-					waiver = sw.get_waiver(summary.waiver_id, True)
-					break
-				except (smartwaiver.exceptions.SmartwaiverHTTPException, smartwaiver.exceptions.SmartwaiverSDKException):
-					print("Smartwaiver Error")
-					sw_fails += 1
-					if sw_fails > 3:
-						raise
-					else:
-						time.sleep(5)
-						continue
+				WA_ID = None
+				print(summary.first_name, summary.last_name)
 
+				for tag in summary.tags:
+					if tag.split(' ')[0]=='WA_ID':
+						WA_ID = int(tag.split(' ')[1])
 
-			contact=None
-			
-			if not WA_ID and summary.is_minor:
-				print("Skipping untagged minor waiver")
-				continue
-
-			else:
+				sw_fails = 0
 				while(1):
 					try:
-						#Pull contact's info from WA if it exists
-						if WA_ID:
-							contact = self.WA_API.GetContactById(WA_ID)
+						waiver = sw.get_waiver(summary.waiver_id, True)
+						break
+					except (smartwaiver.exceptions.SmartwaiverHTTPException, smartwaiver.exceptions.SmartwaiverSDKException):
+						print("Smartwaiver Error")
+						sw_fails += 1
+						if sw_fails > 3:
+							raise
 						else:
-							contact = self.WA_API.GetContactByEmail(waiver.email)[0]
-							WA_ID = contact['Id']
-						break
-						#print(contact)
+							time.sleep(5)
+							continue
 
-					#If query returns no contact
-					except IndexError:
-						print("Contact does not exist")
-						break
-					except TypeError:
-						print("Failed to connect to WA")
-						time.sleep(60)
-						continue
-			
-			if not contact:
-				continue
-			#If waiver date is not newer than what is currently on the WA profile, don't update
-			saved_waiver_date = [field['Value'] for field in contact['FieldValues'] if field['FieldName']=="WaiverDate"][0]
-			print(contact['Email'])
-			print("saved waiver date:", saved_waiver_date)
-			print("summary created_on date:", summary.created_on)
-			if saved_waiver_date and  saved_waiver_date.strip() != '':
-				print("Has waiver date")
-				if datetime.strptime(saved_waiver_date, "%Y-%m-%d %H:%M:%S") >= datetime.strptime(summary.created_on, "%Y-%m-%d %H:%M:%S"):
+
+				contact=None
+				
+				if not WA_ID and summary.is_minor:
+					print("Skipping untagged minor waiver")
 					continue
 
-			#update WA account waiver date with the current waiver's date
-			print('.' + summary.dob + '.')
-			waiver_DOB = datetime.strptime(summary.dob, "%Y-%m-%d")
-			WA_DOB = waiver_DOB.strftime("%d %b %Y")
-			#print(WA_DOB)
-			#print(WA_ID, ":", waiver_date)
-			#print(waiver_DOB)
-			#print(type(WA_ID))
-			#print(type(waiver_date))
-			self.SetWaiverDate(WA_ID, summary.created_on)
-			time.sleep(3)
-			self.SetDOB(WA_ID, summary.dob)
+				else:
+					while(1):
+						try:
+							#Pull contact's info from WA if it exists
+							if WA_ID:
+								contact = self.WA_API.GetContactById(WA_ID)
+							else:
+								contact = self.WA_API.GetContactByEmail(waiver.email)[0]
+								WA_ID = contact['Id']
+							break
+							#print(contact)
 
+						#If query returns no contact
+						except IndexError:
+							print("Contact does not exist")
+							break
+						except TypeError:
+							print("Failed to connect to WA")
+							time.sleep(60)
+							continue
+				
+				if not contact:
+					continue
+				#If waiver date is not newer than what is currently on the WA profile, don't update
+				saved_waiver_date = [field['Value'] for field in contact['FieldValues'] if field['FieldName']=="WaiverDate"][0]
+				waiver_date = datetime.strptime(summary.created_on, "%Y-%m-%d %H:%M:%S").astimezone(timezone.utc)
+				if waiver_date > from_dt:
+					from_dt = waiver_date
+				print(contact)
+				print(contact['Email'])
+				print("saved waiver date:", saved_waiver_date)
+				print("summary created_on date:", summary.created_on)
+				if saved_waiver_date and  saved_waiver_date.strip() != '':
+					saved_waiver_date = datetime.strptime(saved_waiver_date, "%Y-%m-%d %H:%M:%S").astimezone(timezone.utc)
+					print("Has waiver date")
+					if saved_waiver_date >= waiver_date:
+						continue
 
-			
-			#print(waiver.pdf)
+				#update WA account waiver date with the current waiver's date
+				print('.' + summary.dob + '.')
+				waiver_DOB = datetime.strptime(summary.dob, "%Y-%m-%d")
+				WA_DOB = waiver_DOB.strftime("%d %b %Y")
+				#print(WA_DOB)
+				#print(WA_ID, ":", waiver_date)
+				#print(waiver_DOB)
+				#print(type(WA_ID))
+				#print(type(waiver_date))
+				self.SetWaiverDate(WA_ID, summary.created_on)
+				time.sleep(3)
+				self.SetDOB(WA_ID, summary.dob)
+
+				#print(waiver.pdf)
+
+			# We retrieved less than 100 summaries, so there are no more to fetch
+			if len(summaries) < 100:
+				break
 
 if __name__ == "__main__":
 	s = ChildScript("Waiver Check")
