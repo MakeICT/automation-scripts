f72e298ec38cb5b85320ec43ae14d7eb6659c25b
Christian Kindel <iceman81292@gmail.com>
2022-12-06 09:48:46 -0600

    adjust criteria in archive script

--- ArchiveInactive.py
+++ ArchiveInactive.py
@@ -17,20 +17,31 @@ class ChildScript(Script):
         self.processed_filename = "followup_processed.txt"
 
     def Run(self):
-        cutoff_date = self.WA_API.DateTimeToWADate(datetime.now() - timedelta(days=720))
-        inactive_contacts = self.WA_API.GetFilteredContacts("'Archived'+eq+'False'+and+'Member'+eq+'False'+and+'Profile+last+updated'+le+%s+and+'Last+login+date'+le+%s"% (cutoff_date, cutoff_date))
+        cutoff_date = self.WA_API.DateTimeToWADate(datetime.now() - timedelta(days=540))
+        # inactive_contacts = self.WA_API.GetFilteredContacts("'Archived'+eq+'False'+and+'Member'+eq+'False'+and+'Profile+last+updated'+le+%s+and+'Last+login+date'+le+%s"% (cutoff_date, cutoff_date))
+        inactive_contacts = self.WA_API.GetFilteredContacts("'Archived'+eq+'False'+and+'Profile+last+updated'+le+%s+and+'Last+login+date'+le+%s"% (cutoff_date, cutoff_date))
         # for contact in inactive_contacts:
         #     print(contact, '\n')
         print(len(inactive_contacts))
         ids = [contact['Id'] for contact in inactive_contacts]
+        # print(inactive_contacts[27])
         print(ids)
+        # quit()
+        pruned_contacts = []
         for id in ids:
             try:
-                self.WA_API.UpdateContactField(int(id), "Archived",True)
+                contact = self.WA_API.GetContactById(id)
+                level = contact.get('MembershipLevel')
+                if not level or level and level.get('Name') == 'Non-Member':
+                    print(id)
+                    pruned_contacts.append(id)
+                    self.WA_API.UpdateContactField(int(id), "Archived",True)
             except Exception as e:
                 print("issue with user id:", id )
                 print(e)
                 # print(sys.exc_info()[0])
+
+        print(len(pruned_contacts))
 if __name__ == "__main__":
     s = ChildScript("Archive Inactive WA Contacts")
     s.RunAndNotify()
